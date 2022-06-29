bdd165a43adb6441d75481ad40087d46a435746c
Sabrina <n.sabrinaghazali@gmail.com>
2022-06-29 22:26:22 +0800

    penat
    Second newcode commit

---
+++ LabEMS/src/main/java/com/javamaster/dao/ArticleDao.java
@@ -0,0 +1,22 @@
+//Article = Event
+
+package com.javamaster.dao;
+
+
+
+import java.util.List;
+
+import com.javamaster.entity.Article;
+
+public interface ArticleDao {
+
+	int createArticle(Article article);
+	
+	int editArticle(Article article);
+	
+	void deleteArticle(int id);
+	
+	Article getArticleById(int id);
+	
+	List<Article> getAllArticles();
+}
---
+++ LabEMS/src/main/java/com/javamaster/dao/CategoryDao.java
@@ -0,0 +1,16 @@
+//Category = Type
+
+package com.javamaster.dao;
+
+import java.util.List;
+
+import com.javamaster.entity.Category;
+
+public interface CategoryDao {
+
+	int createCategory(Category category);
+	void deleteCategory(Category category);
+	int editCategory(Category category);
+	
+	List<Category> getAllCategories();
+}
