e09cefd63aedb100fb307ccbc1f79d850957fecf
Sabrina <n.sabrinaghazali@gmail.com>
2022-06-29 20:38:55 +0800

    Vnew commit

---
+++ LabISP Yeagerist/src/main/java/Event.java
@@ -0,0 +1,40 @@
+public class Event
+{
+	private int eventId;
+	private String eventName;
+	private String eventType;
+	private int noParticipant;
+	
+	public Event() {}
+	public int getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public int getNoParticipant() {
+		return noParticipant;
+	}
+
+	public void setNoParticipant(int noParticipant) {
+		this.noParticipant = noParticipant;
+	}
+	public String getEventType() {
+		return eventType;
+	}
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+
+
+}
